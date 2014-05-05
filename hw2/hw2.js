41ad02a189d55c2cb8fc7526327957201fbbafab
Egor Miasnikov <e.miasnikov@gmail.com>
2014-05-05 07:20:47 +0300

    change to async method and added setContentType

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -3,40 +3,49 @@
  */
 function API(){
 
-    var xhr = function(type, url, data){
-        return new Promise(function(resolve, reject) {
+    var xhr;
+
+    xhr = function (type, url, data, contentType) {
+        return new Promise(function (resolve, reject) {
             var req = new XMLHttpRequest();
-            req.open(type, url, false);
+            req.open(type, url, true);
+            req.setRequestHeader("Content-Type", contentType);
+            req.onload = function () {
+                if (this.status === 200 && (this.response.charAt(0) === "{" || this.response.charAt(0) === "[")) {
+                    var result = JSON.parse(this.response);
+                    resolve(result);
+                } else {
+                    reject(this.responseText);
+                }
+            };
+            req.onerror = function (e) {
+                reject(e);
+            };
+
             req.send(data);
-            if(req.status === 200){
-                var result = JSON.parse(req.response);
-                resolve(result);
-            }else{
-                reject(req.responseText);
-            }
         });
     };
-    this.get = function(url) {
+
+    this.get = function (url) {
         var type = 'GET',
-            data = false;
-        return xhr(type, url, data);
+            data = false,
+            contentType = 'text/plain; charset=UTF-8';
+        return xhr(type, url, data, contentType);
     };
-    this.query = function(url) {
-        var type = 'GET',
-            data = false;
-        return xhr(type, url, data);
+    this.post = function (url, data) {
+        var type = 'POST',
+            contentType = "application/json;charset=UTF-8";
+        return xhr(type, url, JSON.stringify(data), contentType);
     };
-    this.post = function(url, data){
-        var type = 'POST';
-        return xhr(type, url, JSON.stringify(data));
+    this.put = function (url, data) {
+        var type = 'PUT',
+            contentType = "application/json;charset=UTF-8";
+        return xhr(type, url, JSON.stringify(data), contentType);
     };
-    this.put = function(url, data){
-        var type = 'PUT';
-        return xhr(type, url, JSON.stringify(data));
-    };
-    this.delete = function(url){
+    this.delete = function (url) {
         var type = 'DELETE',
-            data = false;
-        return xhr(type, url, data);
+            data = false,
+            contentType = 'text/plain; charset=UTF-8';
+        return xhr(type, url, data, contentType);
     };
-}
+};
